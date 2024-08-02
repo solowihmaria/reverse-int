c8a5ec4cb8a3148cd5119f781f57880eebcf674c
solowihmaria <solowihmaria@gmail.com>
2024-08-02 13:59:33 +0300

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    
+    let str = Math.abs(n).toString();
+    let result = '';
+    let i = 0;
   
-}
+    while (i < str.length) {
+      result = str[i] + result;
+      i++;
+    }
+  
+    return parseInt(result);
+  };
+  
